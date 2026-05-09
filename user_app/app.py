e225ef4ee0c71e5e7bd65e2c88aaf825a702a26d
EgorPortyanov <egor.portyanov@gmail.com>
2026-05-09 20:43:51 +0300

    Задание 2

--- user_app/app.py
+++ user_app/app.py
@@ -1,3 +1,9 @@
 import flask
 
-user_app = flask.Blueprint(import_name="user_app", name="user_app", template_folder="templates")
+user_app = flask.Blueprint(
+    import_name="user_app", 
+    name="user_app", 
+    template_folder="templates", 
+    static_folder="static",
+    static_url_path="/user_app/static"
+    )
